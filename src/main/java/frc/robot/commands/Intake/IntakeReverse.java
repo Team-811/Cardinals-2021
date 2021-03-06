5433070c5caff826dbf308402e87e755c35ab4c8
Team 811 <team811cardinals@gmail.com>
2021-03-06 13:59:03 -0500

    created the structure for the intake subsystem commands, not finished - :)

--- src/main/java/frc/robot/commands/Intake/IntakeReverse.java
+++ src/main/java/frc/robot/commands/Intake/IntakeReverse.java
@@ -1,17 +1,30 @@
 package frc.robot.commands.Intake;
 
+import java.lang.module.ModuleDescriptor.Requires;
+
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Constants;
+import frc.robot.Robot;
 
+public class IntakeReverse extends InstantCommand{
 
-
-public class IntakeReverse {
     public IntakeReverse() {
-
+        requires(Robot.carousel);
     }
 
     @Override
-    protected void initalize() {
-        Robot.intake.IntakeReverse(Constants.INTAKE_SPEED_REVERSE);
+    protected void initalize() {}
+
+    @Override
+    protected void execute() {
+        Robot.intake.intakeReverse(Constants.INTAKE_SPEED_REVERSE);
+    }
+
+    @Override
+    protected void end() {}
+
+    @Override 
+    protected boolean isFinished() {
+        return false;
     }
 }
