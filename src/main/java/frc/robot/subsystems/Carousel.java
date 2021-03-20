1fad70101a73d6b1893c59977bd2423ba302d5d4
Team 811 <team811cardinals@gmail.com>
2021-03-20 11:47:50 -0400

    Simplified the carousel and intake code, add motor config, made smartdash output - ch

--- src/main/java/frc/robot/subsystems/Carousel.java
+++ src/main/java/frc/robot/subsystems/Carousel.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -17,54 +18,75 @@ public class Carousel extends Subsystem implements ISubsystem {
     }
 
     private TalonSRX carouselMotor;
-    private boolean bSpinning;      // flag to denote if this is spinning currently
-    private boolean bForward;       // direction of the spin (true if foward, false if reverse)
+    private boolean CarouselIsRunning;
+    // private boolean bSpinning;      // flag to denote if this is spinning currently
+    // private boolean bForward;       // direction of the spin (true if foward, false if reverse)
 
     public Carousel() {
         carouselMotor = new TalonSRX(RobotMap.CAROUSEL);
-        resetData();
-        carouselMotor.setInverted(false); // only do this once!
+        //resetData();
+        resetSubsystem();
+    }
+
+    //This says run the carousel at a speed
+    public void toggleCarousel(double speed) {
+        carouselMotor.set(ControlMode.PercentOutput, 0);
+        if (speed == 0){
+            CarouselIsRunning = false;
+        } else {
+            CarouselIsRunning = true;
+        }
+    }
+
+    public void spinCarousel(double speed) {
+        CarouselIsRunning = !CarouselIsRunning;
+        if (CarouselIsRunning){
+            carouselMotor.set(ControlMode.PercentOutput, speed);
+        }else {
+            carouselMotor.set(ControlMode.PercentOutput, 0);
+        }
+    }
+    
+    public void stopCarousel() {
+        CarouselIsRunning = false;
+        carouselMotor.set(ControlMode.PercentOutput, 0);
     }
 
     /** Reset the member data to default values */
-    public void resetData()
-    {
-        setUsage ( false );
-        setDirection ( true  );
-    }
+    // public void resetData() {
+    //     setUsage (false);
+    //     setDirection (true);
+    // }
 
     /** 
      * determine if the unit is spinning/in use
      * @return true if unit is in use
      */
-    public boolean isInUse() {
-        return bSpinning;
-    }
+    // public boolean isInUse() {
+    //     return bSpinning;
+    // }
     /** 
      * set flag to denote unit is in use
      * @param bFlag  true if in use, false if not
      */
-    public void setUsage ( boolean bFlag )
-    {
-        bSpinning = bFlag;
-    }
+    // public void setUsage (boolean bFlag) {
+    //     bSpinning = bFlag;
+    // }
 
     /**
      * Set the direction of unit,while in use
      * @param bIsForward  true if "foward"
-     */
-    public void setDirection ( boolean bIsForward ) 
-    {
-        bForward = bIsForward;
-    }
+    //  */
+    // public void setDirection ( boolean bIsForward ) {
+    //     bForward = bIsForward;
+    // }
     /** 
      * get the direction of the unit in use
      * @return true, if spinning foward, false for reverse
      */
-    public boolean getDirection()
-    {
-        return bForward;
-    }
+    // public boolean getDirection(){
+    //     return bForward;
+    // }
 
 
     /***********************************************************
@@ -72,24 +94,26 @@ public class Carousel extends Subsystem implements ISubsystem {
      *  @param speed          how fast to spin motor, in directio (-1 to 1)
      *  @param motorInversion Direction of motor (See Constants)
      ************************************************************/
-    public void spinCarousel (double speed )
-    {
-        // if not in use, take ownership
-        if ( ! isInUse() )
-        {
-            setUsage ( true );
-            setDirection ( speed > 0.0 );
-            carouselMotor.set(ControlMode.PercentOutput, speed);
-        }
-    }
-    /**
-     * Stop the carousel and reset default data
-     */
-    public void stopCarousel() 
-    {
-        // stop motion
-        resetData ();
-        carouselMotor.set(ControlMode.PercentOutput, 0);
+    // public void spinCarousel (double speed )
+    // {
+    //     // if not in use, take ownership
+    //     if (!isInUse()){
+    //         setUsage (true);
+    //         setDirection (speed > 0.0);
+    //         carouselMotor.set(ControlMode.PercentOutput, speed);
+    //     }
+    // }
+    // /**
+    //  * Stop the carousel and reset default data
+    //  */
+    // public void stopCarousel() {
+    //     resetData ();
+    //     carouselMotor.set(ControlMode.PercentOutput, 0);
+    // }
+
+    private void configureMotorControllers() {
+        carouselMotor.setInverted(true);
+        carouselMotor.setNeutralMode(NeutralMode.Brake); //could be kBrake -- check
     }
 
     @Override
@@ -109,13 +133,14 @@ public class Carousel extends Subsystem implements ISubsystem {
 
     @Override
     public void outputSmartdashboard() {
-        SmartDashboard.putBoolean("Carousel Spin Fwd", getDirection() );
+        SmartDashboard.putBoolean("Carousel Spin Fwd", CarouselIsRunning);
     }
 
     @Override
     public void resetSubsystem() {
         stopCarousel();
         zeroSensors();
+        configureMotorControllers();
     }
 
 }
